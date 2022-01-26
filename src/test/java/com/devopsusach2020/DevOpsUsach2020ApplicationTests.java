5f16e2a5c467ddb6350b58ead5c1d2738403be1b
Alejandro Arevalo <aoarevalo@CLFTC-C02G54TWMD6N.local>
2022-01-26 19:56:00 -0300

    cambios iniciales para después usar gradle

---
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.devopsusach2020;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsUsach2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// NOSONAR
+		assertTrue(true);
+	}
+
+}
