4134989fb2aba8fa9ae19da0156e16cab03a7032
Alejandro Arevalo <aoarevalo@CLFTC-C02G54TWMD6N.local>
2022-02-04 22:57:20 -0300

    update 1

--- maven.groovy
+++ maven.groovy
@@ -18,32 +18,32 @@ def call(){
       sh "mvn spring-boot:run&"
       sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
   }
-  stage("Paso 4: Subir Nexus"){
-      nexusPublisher nexusInstanceId: 'nexus',
-      nexusRepositoryId: 'devops-usach-nexus',
-      packages: [
-          [$class: 'MavenPackage',
-              mavenAssetList: [
-                  [classifier: '',
-                  extension: 'jar',
-                  filePath: 'build/DevOpsUsach2020-0.1.2.jar'
-              ]
-          ],
-              mavenCoordinate: [
-                  artifactId: 'DevOpsUsach2020',
-                  groupId: 'com.devopsusach2020',
-                  packaging: 'jar',
-                  version: '0.1.2'
-              ]
-          ]
-      ]
-  }
-  stage("Paso 5: Descargar Nexus"){
-      sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.1.2/DevOpsUsach2020-0.1.2.jar" -O'
-  }
-  stage("Paso 6: Levantar Artefacto Jar"){
-      sh 'nohup bash java -jar DevOpsUsach2020-0.1.2.jar & >/dev/null'
-  }
+//   stage("Paso 4: Subir Nexus"){
+//       nexusPublisher nexusInstanceId: 'nexus',
+//       nexusRepositoryId: 'devops-usach-nexus',
+//       packages: [
+//           [$class: 'MavenPackage',
+//               mavenAssetList: [
+//                   [classifier: '',
+//                   extension: 'jar',
+//                   filePath: 'build/DevOpsUsach2020-0.0.1.jar'
+//               ]
+//           ],
+//               mavenCoordinate: [
+//                   artifactId: 'DevOpsUsach2020',
+//                   groupId: 'com.devopsusach2020',
+//                   packaging: 'jar',
+//                   version: '0.0.1'
+//               ]
+//           ]
+//       ]
+//   }
+//   stage("Paso 5: Descargar Nexus"){
+//       sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
+//   }
+//   stage("Paso 6: Levantar Artefacto Jar"){
+//       sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
+//   }
   stage("Paso 7: Testear Artefacto - Dormir(Esperar 20sg) "){
       sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
   }
