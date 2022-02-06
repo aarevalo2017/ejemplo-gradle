89ea9a4fadf1edc82f9c81af6955b0bc3b8051ae
Alejandro Arevalo <aoarevalo@CLFTC-C02G54TWMD6N.local>
2022-02-06 02:12:03 -0300

    remove bash from run jar

--- pipelinecd.groovy
+++ pipelinecd.groovy
@@ -10,7 +10,7 @@ def call(){
       sh "curl -X GET -u $NEXUS_USERNAME:$NEXUS_PASSWORD 'http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.1.2/DevOpsUsach2020-0.1.2.jar' -O"
   }
   stage("Paso 6: Levantar Artefacto Jar"){
-      sh 'nohup bash java -jar DevOpsUsach2020-0.1.2.jar & >/dev/null'
+      sh 'nohup java -jar DevOpsUsach2020-0.1.2.jar & >/dev/null'
   }
   stage("Paso 7: Testear Artefacto - Dormir(Esperar 20sg) "){
       sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
